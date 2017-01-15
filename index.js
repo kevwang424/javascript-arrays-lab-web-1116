b486f6a87375a6a27919173a3305ee4b70f4084c
kevwang424 <kev.wang424@gmail.com>
2017-01-15 15:17:44 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 const app = "I don't do much."
+
+function destructivelyAppendKitten(name){
+  kittens.push(name);
+}
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop();
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift();
+}
+
+function appendKitten(name){
+  const answer = [...kittens, name];
+  return answer;
+}
+
+function prependKitten(name){
+  const answer = [name, ...kittens];
+  return answer;
+}
+
+function removeLastKitten(){
+  const answer = kittens.slice(0, kittens.length - 1);
+  return answer;
+}
+
+function removeFirstKitten(){
+  const answer = kittens.slice(1);
+  return answer;
+}
